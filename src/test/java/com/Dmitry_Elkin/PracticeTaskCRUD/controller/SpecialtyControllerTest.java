9c3bbbaa0c9e96c628c2b9fbdd8178bf75a428fd
Dmitry757 <43566694+Dmitry757@users.noreply.github.com>
2023-01-31 00:43:38 +0300

    some attempt to work with powermock

--- src/test/java/com/Dmitry_Elkin/PracticeTaskCRUD/controller/SpecialtyControllerTest.java
+++ src/test/java/com/Dmitry_Elkin/PracticeTaskCRUD/controller/SpecialtyControllerTest.java
@@ -1,25 +1,36 @@
 package com.Dmitry_Elkin.PracticeTaskCRUD.controller;
 
-import com.Dmitry_Elkin.PracticeTaskCRUD.model.Specialty;
 
+import com.Dmitry_Elkin.PracticeTaskCRUD.model.Specialty;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.powermock.api.mockito.PowerMockito.spy;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
+@RunWith(PowerMockRunner.class)
 class SpecialtyControllerTest {
 
     @Test
-    void createNewItem(){
+    void printItems(){
 
-        SpecialtyController specialtyController = Mockito.mock(SpecialtyController.class);
+//        SpecialtyController scMock = Mockito.mock(SpecialtyController.class);
 
+        SpecialtyController scMock = spy(new SpecialtyController());
         Specialty specialty = new Specialty("Test");
+        try {
+            when(scMock,"printItems").thenReturn(specialty);
+        } catch (Exception e) {
+            System.out.println("exception! "+e.getMessage());
+        }
 
 //        Mockito.when(specialtyController.createNewItem())
-
 //        mockRepository.addOrUpdate(specialty);
     }
 
